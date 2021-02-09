9dbd36db2198a6f1369846c9aecc42b611ee8338
NwaeNandarAung <scm.nwaenandaraung@gmail.com>
2021-02-09 10:03:53 +0630

    Change Post Route

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   <div class="container">
     <h3 align="center" style="margin-top:20px; color:#999;">Create Post</h3><br/>
-    <form class="form-horizontal" method="POST" action="{{ route('createconfirm') }}">
+    <form class="form-horizontal" method="POST" action="{{ route('post') }}">
       {{ csrf_field() }}
       <div class="form-group row">
         <label for="title" class="offset-sm-2 col-sm-2 col-form-label">Title</label>
